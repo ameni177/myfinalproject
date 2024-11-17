f9ada3b7330777d1a0aba46b81ddaf42412ca4ca
ameni <ameni.kallel@edu.techstarter.de>
2024-11-17 19:15:56 +0100

    cccc

--- main.tf
+++ main.tf
@@ -7,15 +7,15 @@ resource "aws_instance" "web8" {
   instance_type = "t2.micro"
   key_name      = "NEW"
 
-  security_groups = [aws_security_group.web_sg2.name]
+  security_groups = [aws_security_group.web_sg8.name]
 
   tags = {
     Name = "web-server8"
   }
 }
 
-resource "aws_security_group" "web_sg2" {
-  name        = "web-sg2"
+resource "aws_security_group" "web_sg8" {
+  name        = "web-sg8"
   description = "Allow inbound traffic to EC2 instance"
 
   ingress {
