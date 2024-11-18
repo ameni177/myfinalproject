1e19f958bd4a5869c01d4c7111305b4cd8dd0775
ameni <ameni.kallel@edu.techstarter.de>
2024-11-18 12:41:22 +0100

    final1

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ provider "aws" {
 }
 
 
-resource "aws_security_group" "web_sg26" {
-  name        = "web-sg26"
+resource "aws_security_group" "web_sg27" {
+  name        = "web-sg27"
   description = "Allow inbound traffic to EC2 instance"
 
   ingress {
